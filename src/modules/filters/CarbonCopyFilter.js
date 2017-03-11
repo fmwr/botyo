144fcef38513df3420afe49ff78c3ec6a929e80c
Tomasz Wróbel <tomekwr@gmail.com>
2017-03-11 20:58:01 +0100

    Allow mentioning multiple users in a single message
    This method is currently way too big, needs to be refactored

--- src/modules/filters/CarbonCopyFilter.js
+++ src/modules/filters/CarbonCopyFilter.js
@@ -5,6 +5,22 @@ import ChatApi from "../../core/api/ChatApi";
 import Promise from "bluebird";
 import { remove } from "diacritics";
 
+function failureText(handle, reason) {
+    return `\u{2757}\u{FE0F} użytkownik ${handle} - ${reason}`;
+}
+
+function successText(handle, targetName) {
+    return `\u{2714}\u{FE0F} użytkownik ${handle} powiadomiony (${targetName})`;
+}
+
+function notificationText(senderName, threadName, content) {
+    return (
+        `\u{1F4E2} Powiadomienie od: ${senderName}\n` +
+        `\u{1F4E5} W wątku: ${threadName}\n\n` +
+        `${content}`
+    );
+}
+
 @Singleton
 @Inject(ChatApi, Threads)
 export default class CarbonCopyFilter extends FilterModule {
@@ -24,44 +40,46 @@ export default class CarbonCopyFilter extends FilterModule {
 
         if (!pingExpressions) return msg;
 
-        const username = pingExpressions[0].slice(1);
-        const targetId = this.threads.getUserIdByThreadIdAndAlias(msg.threadID, username);
-
-        if (!targetId) {
-            this.api.sendMessage("Użytkownik nierozpoznany", msg.threadID);
-            return msg;
-        }
-
-        const senderId = parseInt(msg.senderID.split("fbid:")[1] || msg.senderID);
-        
-        if (!senderId) throw new Error("Could not get senderId");
-
-        const threadNamePromise = this.api.getThreadInfo(msg.threadID)
-            .then(info => info.name || Promise.reject("No thread name"));
-        const senderNamePromise = this.api.getUserInfo(senderId)
-            .then(info => info[senderId].name || Promise.reject("Could not get sender name", info));
-        const targetNamePromise = this.api.getUserInfo(targetId)
-            .then(info => info[targetId].firstName || Promise.reject("Could not get target name", info));
-
         Promise
-            .all([targetNamePromise, threadNamePromise, senderNamePromise])
-            .then(([targetName, threadName, senderName]) => {
-                return [
-                    targetName,
-                    this.api.sendMessage({
-                        body:
-                            `\u{1F4E2} Powiadomienie od: ${senderName}\n` +
-                            `\u{1F4E5} W wątku: ${threadName}\n\n` +
-                            `${ msg.body }`
-                    }, targetId)
-                ];
-            })
-            .all()
-            .then(
-                ([targetName]) => {
-                    return this.api.sendMessage(`\u{2714}\u{FE0F} Użytkownik ${targetName} został powiadomiony!`, msg.threadID)    
-                }
-            );
+            .all( pingExpressions.map( pingExpression => {
+
+                return new Promise( (resolve, reject) => {
+
+                    const username = pingExpression.slice(1);
+
+                    const targetId = this.threads.getUserIdByThreadIdAndAlias(msg.threadID, username);
+                    if (!targetId) {
+                        resolve(failureText(username, "nierozpoznany"));
+                        return;
+                    }
+
+                    const senderId = parseInt(msg.senderID.split("fbid:")[1] || msg.senderID);
+                    if (!senderId) {
+                        resolve(failureText(username, "could not get senderId"));
+                        return;
+                    }
+
+                    const threadNamePromise = this.api.getThreadInfo(msg.threadID).then(info => info.name || Promise.reject("No thread name"));
+                    const senderNamePromise = this.api.getUserInfo(senderId).then(info => info[senderId].name || Promise.reject("Could not get sender name", info));
+                    const targetNamePromise = this.api.getUserInfo(targetId).then(info => info[targetId].firstName || Promise.reject("Could not get target name", info));
+
+                    Promise
+                        .all([targetNamePromise, threadNamePromise, senderNamePromise])
+                        .then(([targetName, threadName, senderName]) => [
+                            targetName,
+                            this.api.sendMessage(notificationText(senderName, threadName, msg.body), targetId)
+                        ])
+                        .all()
+                        .then(
+                            ([targetName]) => resolve(successText(username, targetName)),
+                            error          => resolve(failureText(username, "nieznany błąd"))
+                        );
+
+                });
+
+            }))
+            .then(results => this.api.sendMessage(results.join("\n"), msg.threadID));
+
 
         return msg;
     }
